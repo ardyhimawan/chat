50d56006b84f13b17b6f32eff2de52427c4a5476
ardyhim <ardyhim@yahoo.com>
2016-01-28 19:06:39 -0800

    chat
    chat menggunakan express generator dan socket.io

--- routes/socket.js
+++ routes/socket.js
@@ -1,10 +1,27 @@
-module.exports = function(io) {
+var express = require('express');
+var router = express.Router();
+var linkifyHtml = require('linkifyjs/html');
+var linkifyStr = require('linkifyjs/string');
+
+function socket(io) {
   // start listen with socket.io
   io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('new message', function(msg){
-      console.log('new message: ' + msg);
-      io.emit('chat message', msg);
+      var options = {/* … */};
+      var data = {
+        nama: linkifyStr(msg.nama, options),
+        message: linkifyStr(msg.message, options)
+      }
+      io.emit('chat message', data);
     });
   });
+  /* GET home page. */
+  router.get('/home', function(req, res, next) {
+    res.render('index', { title: 'Express socket' });
+  });
+}
+module.exports = {
+  router: router,
+  sck: socket
 }
